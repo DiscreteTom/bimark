dabd818489cd3d5fad64ce31770d748b0cac155d
DiscreteTom <discrete_tom@outlook.com>
2023-03-30 10:00:59 +0800

    chore(bimark): simplify render with findTextNodes

--- src/bimark.ts
+++ src/bimark.ts
@@ -80,45 +80,40 @@ export class BiMark extends BiDoc {
    * Render a markdown file based on the collected definitions.
    */
   render(path: string, md: string, options?: BiMarkRenderOptions) {
-    const ast = remark.parse(md);
-    visit(ast, (node) => {
-      if ("children" in node) {
-        node.children = node.children.map((c) => {
-          if (c.type == "text") {
-            const { type, value, ...rest } = c;
-            return {
-              type: "html", // use html node to avoid escaping
-              value: this.renderText(
-                path,
-                c.value,
-                c.position!,
-                // def renderer
-                (d) =>
-                  `<span id="${d.id}">${
-                    (options?.def?.showBrackets ? "[[" : "") +
-                    d.name +
-                    (options?.def?.showAlias && d.alias.length > 0
-                      ? "|" + d.alias.join("|")
-                      : "") +
-                    (options?.def?.showBrackets ? "]]" : "")
-                  }</span>`,
-                // ref renderer
-                (ref) => {
-                  const span = `<span id="${this.refIdGenerator(ref)}">${
-                    (options?.ref?.showBrackets ? "[[" : "") +
-                    ref.name + // don't use def.name here, because it may be an alias
-                    (options?.ref?.showBrackets ? "]]" : "")
-                  }</span>`;
-                  return options?.ref?.html || options?.output?.html
-                    ? `<a href="${ref.def.path}#${ref.def.id}">${span}</a>`
-                    : `[${span}](${ref.def.path}#${ref.def.id})`;
-                }
-              ),
-              ...rest,
-            };
-          } else return c;
-        });
-      }
+    const { nodes, ast } = this.findTextNodes(md);
+
+    nodes.forEach(({ node: c, index: i, parent }) => {
+      const { type, value, ...rest } = c;
+      parent.children[i] = {
+        type: "html", // use html node to avoid escaping
+        value: this.renderText(
+          path,
+          c.value,
+          c.position!,
+          // def renderer
+          (d) =>
+            `<span id="${d.id}">${
+              (options?.def?.showBrackets ? "[[" : "") +
+              d.name +
+              (options?.def?.showAlias && d.alias.length > 0
+                ? "|" + d.alias.join("|")
+                : "") +
+              (options?.def?.showBrackets ? "]]" : "")
+            }</span>`,
+          // ref renderer
+          (ref) => {
+            const span = `<span id="${this.refIdGenerator(ref)}">${
+              (options?.ref?.showBrackets ? "[[" : "") +
+              ref.name + // don't use def.name here, because it may be an alias
+              (options?.ref?.showBrackets ? "]]" : "")
+            }</span>`;
+            return options?.ref?.html || options?.output?.html
+              ? `<a href="${ref.def.path}#${ref.def.id}">${span}</a>`
+              : `[${span}](${ref.def.path}#${ref.def.id})`;
+          }
+        ),
+        ...rest,
+      };
     });
 
     if (options?.output?.html)
